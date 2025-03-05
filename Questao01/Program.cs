dab44b714b9b63d4024df0a0dcd5a645c138c0f8
Rachel Pizane <rachel.pzm@gmail.com>
2025-03-04 21:10:41 -0300

    questao 09 feita

---
+++ Questao01/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dataNascimento = ObterData("Informe sua data de nascimento (dd/mm/yyyy): ");
+            DateTime dataAtual = DateTime.Today;
+
+            int anos = CalcularAnos(dataNascimento, dataAtual);
+            int meses = CalcularMeses(dataNascimento, dataAtual);
+            int dias = CalcularDias(dataNascimento, dataAtual);
+
+            Console.WriteLine("== SUA IDADE EXATA ===============================");
+            Console.WriteLine($"Você tem {anos} ano(s), {meses} mes(es) e {dias} dia(s) de idade.");
+        }
+
+        static DateTime ObterData(string mensagemInput)
+        {
+            Console.Write(mensagemInput);
+            string sData = Console.ReadLine();
+            DateTime data;
+
+
+            while (!DateTime.TryParse(sData, out data) || data > DateTime.Today)
+            {
+                Console.WriteLine("Data inválida. Tentar novamente.");
+
+                Console.Write(mensagemInput);
+                sData = Console.ReadLine();
+            }
+
+            return data;
+        }
+
+        static int CalcularAnos(DateTime dataNascimento, DateTime dataAtual)
+        {
+            int anos = dataAtual.Year - dataNascimento.Year;
+
+            if (dataAtual.Month < dataNascimento.Month || (dataAtual.Month == dataNascimento.Month && dataAtual.Day < dataNascimento.Day))
+            {
+                anos--;
+            }
+
+            return anos;
+        }
+
+        static int CalcularMeses(DateTime dataNascimento, DateTime dataAtual)
+        {
+            int meses = dataAtual.Month - dataNascimento.Month;
+
+            if (meses < 0)
+            {
+                meses += 12;
+            }
+
+            if (dataAtual.Day < dataNascimento.Day) 
+            {
+                meses--;
+            }
+
+            return meses;
+        }
+
+        static int CalcularDias(DateTime dataNascimento, DateTime dataAtual)
+        {
+            int dias = dataAtual.Day - dataNascimento.Day;
+
+            if (dias < 0)
+            {
+                DateTime ultimoMes = dataAtual.AddMonths(-1);
+                dias += DateTime.DaysInMonth(ultimoMes.Year, ultimoMes.Month);
+            }
+
+            return dias;
+        }
+    }
+}
---
+++ Questao09/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Questao09
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            Console.Write("Digite o salário bruto: ");
+            string sSalarioBruto = Console.ReadLine();
+
+            if (!ValidarDecimal(sSalarioBruto))
+            {
+                Console.WriteLine("Salário bruto inválido");
+                return;
+            }
+
+            double salarioBruto = ConverterStringParaDouble(sSalarioBruto);
+
+
+            double descontoImposto = CalcularImposto(salarioBruto);
+
+        
+            double salarioLiquido = salarioBruto - descontoImposto;
+
+            Console.WriteLine("== CALCULADORA DE SALARIO LIQUIDO ==============");
+            Console.WriteLine($"\nSalário Bruto: R$ {salarioBruto:F2}");
+            Console.WriteLine($"Desconto de Imposto: R$ {descontoImposto:F2}");
+            Console.WriteLine($"Salário Líquido: R$ {salarioLiquido:F2}");
+        }
+
+        static bool ValidarDecimal(string numero)
+        {
+            return Regex.IsMatch(numero, @"^\d+(.\d+)?$") && ConverterStringParaDouble(numero) > 0;
+        }
+
+        static double ConverterStringParaDouble(string numero)
+        {
+            if (numero.Contains("."))
+            {
+                return double.Parse(numero, CultureInfo.InvariantCulture);
+            }
+
+            return double.Parse(numero);
+        }
+
+        static double CalcularImposto(double salario)
+        {
+            const double FAIXA_1 = 2259.20;
+            const double FAIXA_2 = 2826.65;
+            const double FAIXA_3 = 3751.05;
+            const double FAIXA_4 = 4664.68;
+
+            const double ALIQUOTA_1 = 0.075;  
+            const double ALIQUOTA_2 = 0.15;   
+            const double ALIQUOTA_3 = 0.225;  
+            const double ALIQUOTA_4 = 0.275; 
+
+            double imposto = 0;
+
+            if (salario <= FAIXA_1)
+            {
+                imposto = 0;
+            }
+            else if (salario <= FAIXA_2)
+            {
+                imposto = (salario - FAIXA_1) * ALIQUOTA_1;
+            }
+            else if (salario <= FAIXA_3)
+            {
+                imposto = (salario - FAIXA_2) * ALIQUOTA_2 + (FAIXA_2 - FAIXA_1) * ALIQUOTA_1;
+            }
+            else if (salario <= FAIXA_4)
+            {
+                imposto = (salario - FAIXA_3) * ALIQUOTA_3 + (FAIXA_3 - FAIXA_2) * ALIQUOTA_2 + (FAIXA_2 - FAIXA_1) * ALIQUOTA_1;
+            }
+            else
+            {
+                imposto = (salario - FAIXA_4) * ALIQUOTA_4 + (FAIXA_4 - FAIXA_3) * ALIQUOTA_3 + (FAIXA_3 - FAIXA_2) * ALIQUOTA_2 + (FAIXA_2 - FAIXA_1) * ALIQUOTA_1;
+            }
+
+            return imposto;
+        }
+    }
+}
